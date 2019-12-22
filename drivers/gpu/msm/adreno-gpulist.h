d7ced4187ce6e0d0095bacf6e560670f95040ce6
celtare21 <celtare21@gmail.com>
2019-12-22 16:07:30 +0000

    Revert "msm: kgsl: Omit code for GPUs other than Adreno 640"
    This reverts commit 17f799eb0e013fbb243c7ed922027ccb056be714.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -14,7 +14,6 @@
 #define ANY_ID (~0)
 
 static const struct adreno_gpu_core adreno_gpulist[] = {
-#if 0
 	{
 		.gpurev = ADRENO_REV_A306,
 		.core = 3,
@@ -400,7 +399,6 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.gpmu_major = 0x1,
 		.gpmu_minor = 0x007,
 	},
-#endif
 	{
 		.gpurev = ADRENO_REV_A640,
 		.core = 6,
@@ -445,7 +443,6 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.gpmu_tsens = 0x000C000D,
 		.max_power = 5448,
 	},
-#if 0
 	{
 		.gpurev = ADRENO_REV_A680,
 		.core = 6,
@@ -518,5 +515,4 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.busy_mask = 0xFFFFFFFE,
 		.cx_ipeak_gpu_freq = 900000000,
 	},
-#endif
 };
